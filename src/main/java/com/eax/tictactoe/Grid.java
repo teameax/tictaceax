c81e70fc98dac9c7d31d47234ac4b769f55e75ab
Marteinn <marsi2103@gmail.com>
2013-11-23 17:19:40 +0000

    Commits added to java files

--- src/main/java/com/eax/tictactoe/Grid.java
+++ src/main/java/com/eax/tictactoe/Grid.java
@@ -4,7 +4,8 @@ package com.eax.tictactoe;
 public class Grid {
 	
 	private int[] grid = new int[9];
-
+	
+	//Initialize game board
 	public Grid() {
 		for(int i = 0; i < 9; i++)
 			grid[i] = 0;
@@ -14,17 +15,19 @@ public class Grid {
 	{
 		return grid;
 	}
+
+	//Mark a space in the grid
 	public int gridInsert(int player, int coordinate) throws IllegalArgumentException
 	{
-		if(player != 1 && player != 2) //Invalid input, please try again
+		if(player != 1 && player != 2)
 		{
 			throw new IllegalArgumentException("Invalid player number");
 		}
-		else if(coordinate < 0 || coordinate > 8) //Out of bounds
+		else if(coordinate < 0 || coordinate > 8) 
 		{
 			throw new IllegalArgumentException("Invalid grid number");
 		}
-		else if(grid[coordinate] == 1 || grid[coordinate] == 2) //Used coordinate
+		else if(grid[coordinate] == 1 || grid[coordinate] == 2)
 		{
 			return -1;
 		}
